593ccfcd2a6dcbdee7a976557b268d04f85b4aa3
B1rtek <53182944+B1rtek@users.noreply.github.com>
2024-01-26 23:54:57 +0100

    Actually looks good now but still displays a hardcoded string

--- src/LevelInfoLayer.cpp
+++ src/LevelInfoLayer.cpp
@@ -4,20 +4,19 @@
 using namespace geode::prelude;
 
 class $modify(LevelInfoLayer) {
-
-    bool init(GJGameLevel* p0, bool p1) {
+    bool init(GJGameLevel *p0, bool p1) {
         if (!LevelInfoLayer::init(p0, p1)) return false;
 
         auto starsLabel = m_starsLabel;
         bool isDemon = std::stoi(m_starsLabel->getString()) == 10;
-        if(starsLabel && isDemon) {
-            auto bmFont = CCLabelBMFont::create("Tier 69", "bigFont.fnt");
-            bmFont->setID("gddl-rating"_spr);
-            bmFont->setPosition({starsLabel->getPositionX(), starsLabel->getPositionY()}); //193 - 185
-            bmFont->setAnchorPoint({0,1});
-            // bmFont->setScale(0.325f);
-            addChild(bmFont);
-            starsLabel->setPositionY(starsLabel->getPositionY() + 10.f);
+        if (starsLabel && isDemon) {
+            auto tierLabel = CCLabelBMFont::create("Tier 69", "bigFont.fnt");
+            tierLabel->setID("gddl-rating"_spr);
+            float labelShift = starsLabel->getContentSize().height / 2.0f;
+            tierLabel->setPosition({starsLabel->getPositionX(), starsLabel->getPositionY() - labelShift}); //160x185
+            tierLabel->setAnchorPoint({0.5f, 0.5f});
+            tierLabel->setScale(0.4f);
+            addChild(tierLabel);
         }
 
         return true;
