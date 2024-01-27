c9de9d9e0848d7d7f2a4fbdeece1478202f6e4f7
B1rtek <53182944+B1rtek@users.noreply.github.com>
2024-01-27 16:58:05 +0100

    Removed NPE when parsing json from GDDL API

--- src/GDDLRating.cpp
+++ src/GDDLRating.cpp
@@ -1,9 +1,9 @@
 #include "GDDLRating.h"
 
 GDDLRating::GDDLRating(json levelData) {
-    this->rating = levelData["Rating"];
-    this->enjoyment = levelData["Enjoyment"];
-    this->deviation = levelData["Deviation"];
+    this->rating = !levelData["Rating"].is_null() ? static_cast<float>(levelData["Rating"]) : -1.0f;
+    this->enjoyment = !levelData["Enjoyment"].is_null() ? static_cast<float>(levelData["Enjoyment"]) : -1.0f;
+    this->deviation = !levelData["Deviation"].is_null() ? static_cast<double>(levelData["Deviation"]) : 0.0;
     this->ratingCount = levelData["RatingCount"];
     this->enjoymentCount = levelData["EnjoymentCount"];
     this->submissionCount = levelData["SubmissionCount"];
