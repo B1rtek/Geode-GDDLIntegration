37bb23ab12231eae6dc06484c5f718ed60b264fc
B1rtek <53182944+B1rtek@users.noreply.github.com>
2024-01-27 01:25:18 +0100

    Created GDDLRating and added nlohmann/json

---
+++ src/GDDLRating.cpp
@@ -0,0 +1,10 @@
+#include "GDDLRating.h"
+
+GDDLRating::GDDLRating(json levelData) {
+    this->rating = levelData["Rating"];
+    this->enjoyment = levelData["Enjoyment"];
+    this->deviation = levelData["Deviation"];
+    this->ratingCount = levelData["RatingCount"];
+    this->enjoymentCount = levelData["EnjoymentCount"];
+    this->submissionCount = levelData["SubmissionCount"];
+}
---
+++ src/GDDLRating.h
@@ -0,0 +1,21 @@
+#ifndef GDDLRATING_H
+#define GDDLRATING_H
+
+#include <json.hpp>
+
+using json = nlohmann::json;
+
+class GDDLRating {
+public:
+    explicit GDDLRating(json levelData);
+
+    float rating;
+    float enjoyment;
+    double deviation;
+    int ratingCount;
+    int enjoymentCount;
+    int submissionCount;
+};
+
+
+#endif //GDDLRATING_H
