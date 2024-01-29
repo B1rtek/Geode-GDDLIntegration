eb855ffc2b3810b310043f96db6698f9c104f607
B1rtek <53182944+B1rtek@users.noreply.github.com>
2024-01-29 01:25:59 +0100

    Removed unused functions

--- src/RatingsManager.h
+++ src/RatingsManager.h
@@ -9,19 +9,14 @@
 
 class RatingsManager {
     static std::map<int, GDDLRating> demonMap;
-    static std::vector<int> tierColors;
 
     static GDDLRating parseJson(std::string response);
-
-    static cocos2d::ccColor3B convertToColor(int hexColor);
 
 public:
     static int getDemonTier(int id);
     static std::optional<GDDLRating> getRating(int id);
 
     static std::string getRequestUrl(int id);
-
-    static cocos2d::ccColor3B getTierColor(int tier);
 
     static bool addRatingFromResponse(int id, std::string response);
 };
