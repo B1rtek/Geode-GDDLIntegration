bba6dd91058e81d162ec3793e287a37e2a6f3c5f
B1rtek <53182944+B1rtek@users.noreply.github.com>
2024-02-01 02:10:50 +0100

    Autogenerating layout works! (it's clear that I'm not a frontend dev)

--- src/GDDLDemonSplitLayer.cpp
+++ src/GDDLDemonSplitLayer.cpp
@@ -1,9 +1,12 @@
 #include "GDDLDemonSplitLayer.h"
 
+
+#include "RatingsManager.h"
+
 bool GDDLDemonSplitLayer::init() {
     if(!FLAlertLayer::init(75)) return false; // that magic number is actualy bg opacity btw
 
-    CCPoint popupSize = {360.0f, 180.0f};
+    CCPoint popupSize = {360.0f, 200.0f};
     auto winSize = CCDirector::sharedDirector()->getWinSize();
     // background
     auto bg = CCScale9Sprite::create("GJ_square02.png", {0.0f, 0.0f, 80.0f, 80.0f});
@@ -22,13 +25,24 @@ bool GDDLDemonSplitLayer::init() {
     m_buttonMenu->addChild(title);
     m_buttonMenu->reorderChild(title, 0);
     // this is where the tiers go
-
+    for (int row = 0; row < 5; row++) {
+        auto rowNode = CCNode::create();
+        rowNode->setLayout(RowLayout::create()->setGap(5.0f));
+        rowNode->setContentSize({350.0f, 20.0f});
+        for (int column = 0; column < 7; column++) {
+            auto tierNode = createTierNode(row+1+column*5);
+            rowNode->addChild(tierNode);
+        }
+        rowNode->updateLayout();
+        m_buttonMenu->addChild(rowNode);
+        m_buttonMenu->reorderChild(rowNode, row+1);
+    }
     // ok button
     auto spr = ButtonSprite::create("OK");
     auto okButton = CCMenuItemSpriteExtra::create(spr, this, menu_selector(GDDLDemonSplitLayer::onClose));
     okButton->setID("gddl-demon-split-ok"_spr);
     m_buttonMenu->addChild(okButton);
-    m_buttonMenu->reorderChild(okButton, 1);
+    m_buttonMenu->reorderChild(okButton, 10);
 
     m_buttonMenu->updateLayout();
     m_mainLayer->updateLayout();
@@ -38,6 +52,32 @@ bool GDDLDemonSplitLayer::init() {
 void GDDLDemonSplitLayer::onClose(cocos2d::CCObject *sender) {
     setKeypadEnabled(false);
     removeFromParentAndCleanup(true);
+}
+
+CCNode *GDDLDemonSplitLayer::createTierNode(int tier) {
+    auto tierNode = CCNode::create();
+    tierNode->setLayout(RowLayout::create()->setGap(3.0f)->setAutoScale(true));
+    tierNode->setContentSize({50.0f, 20.0f});
+    // tier sprite
+    std::string tierSpriteName = "tier_" + std::to_string(tier) + ".png";
+    auto textureName = Mod::get()->expandSpriteName(tierSpriteName.c_str());
+    auto tierSprite = CCSprite::create(textureName);
+    tierSprite->setScale(0.05f);
+    tierSprite->setContentSize({20.0f, 20.0f});
+    tierNode->addChild(tierSprite);
+    // tier count
+    int count = RatingsManager::getTierStats()[tier];
+    auto countLabel = CCLabelBMFont::create(std::to_string(count).c_str(), "chatFont.fnt");
+    countLabel->setContentSize({30.0f, 20.0f});
+    tierNode->addChild(countLabel);
+    tierNode->updateLayout();
+    // sprite size fix
+    tierSprite->setScale(0.15f);
+    tierSprite->setPosition({4.0f, 1.5f});
+    if(count >= 1000) {
+        countLabel->setScale(0.8f);
+    }
+    return tierNode;
 }
 
 GDDLDemonSplitLayer *GDDLDemonSplitLayer::create() {
