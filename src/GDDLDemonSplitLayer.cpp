b5c8e31f575bfc84544268248e8ac7faa0cf7ac0
B1rtek <53182944+B1rtek@users.noreply.github.com>
2024-02-01 01:05:30 +0100

    I'm too lazy so the game will set the layout for me ez

--- src/GDDLDemonSplitLayer.cpp
+++ src/GDDLDemonSplitLayer.cpp
@@ -1,30 +1,37 @@
 #include "GDDLDemonSplitLayer.h"
 
 bool GDDLDemonSplitLayer::init() {
-    if(!FLAlertLayer::init(0)) return false; // that magic number is actualy bg opacity btw
+    if(!FLAlertLayer::init(75)) return false; // that magic number is actualy bg opacity btw
 
-
-
+    CCPoint popupSize = {360.0f, 180.0f};
+    auto winSize = CCDirector::sharedDirector()->getWinSize();
+    // background
+    auto bg = CCScale9Sprite::create("GJ_square02.png", {0.0f, 0.0f, 80.0f, 80.0f});
+    bg->setContentSize(popupSize);
+    bg->setPosition({ winSize.width / 2, winSize.height / 2 });
+    bg->setID("gddl-demon-split-bg"_spr);
+    m_mainLayer->addChild(bg, -1);
+    // menu with the main layout
     m_buttonMenu = cocos2d::CCMenu::create();
     m_buttonMenu->setID("button-menu"_spr);
+    m_buttonMenu->setLayout(ColumnLayout::create()->setGap(5.0f)->setAxisReverse(true)->setAutoScale(true));
     m_mainLayer->addChild(m_buttonMenu, 10);
+    // title
+    auto title = CCLabelBMFont::create("GDDL Demon Split", "goldFont.fnt");
+    title->setID("gddl-demon-split-title"_spr);
+    m_buttonMenu->addChild(title);
+    m_buttonMenu->reorderChild(title, 0);
+    // this is where the tiers go
 
-    auto userName = cocos2d::CCLabelBMFont::create("gddl test", "bigFont.fnt");
-    userName->setPosition({0, 0});
-    userName->setScale(0.5f);
-    userName->setID("title-label"_spr);
-    m_buttonMenu->addChild(userName);
+    // ok button
+    auto spr = ButtonSprite::create("OK");
+    auto okButton = CCMenuItemSpriteExtra::create(spr, this, menu_selector(GDDLDemonSplitLayer::onClose));
+    okButton->setID("gddl-demon-split-ok"_spr);
+    m_buttonMenu->addChild(okButton);
+    m_buttonMenu->reorderChild(okButton, 1);
 
-    auto buttonSprite = cocos2d::CCSprite::createWithSpriteFrameName("GJ_closeBtn_001.png");
-    buttonSprite->setScale(0.5f);
-    auto button = CCMenuItemSpriteExtra::create(
-        buttonSprite,
-        this,
-        menu_selector(GDDLDemonSplitLayer::onClose)
-    );
-    m_buttonMenu->addChild(button);
-    button->setPosition({30.0f, 30.0f});
-
+    m_buttonMenu->updateLayout();
+    m_mainLayer->updateLayout();
     return true;
 }
 
