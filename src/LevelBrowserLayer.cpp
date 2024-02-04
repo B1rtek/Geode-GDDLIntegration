3c3f3acd2de25ba5d91f6103e1c9d441982f2cf9
B1rtek <53182944+B1rtek@users.noreply.github.com>
2024-02-04 03:07:49 +0100

    Primitive version of GDDL tier search

---
+++ src/LevelBrowserLayer.cpp
@@ -0,0 +1,28 @@
+#include <Geode/Bindings.hpp>
+#include <Geode/modify/LevelBrowserLayer.hpp>
+
+
+#include "RatingsManager.h"
+
+class $modify(GDDLBrowserLayer, LevelBrowserLayer) {
+    int currentPage = 1;
+
+    bool init(GJSearchObject* p0) {
+        if(!RatingsManager::isSearchingForTier() || p0->m_searchType != SearchType::Type19) {
+            return LevelBrowserLayer::init(p0);
+        }
+        return LevelBrowserLayer::init(RatingsManager::getSearchPage(m_fields->currentPage));
+    }
+
+    void onNextPage(CCObject* sender) {
+        LevelBrowserLayer::onNextPage(sender);
+        m_fields->currentPage++;
+        loadPage(RatingsManager::getSearchPage(m_fields->currentPage));
+    }
+
+    void onPrevPage(CCObject* sender) {
+        LevelBrowserLayer::onNextPage(sender);
+        m_fields->currentPage = std::max(0, m_fields->currentPage-1);
+        loadPage(RatingsManager::getSearchPage(m_fields->currentPage));
+    }
+};
