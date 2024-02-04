e44faf26f94fab3917b560d0353e15afead9d1e8
B1rtek <53182944+B1rtek@users.noreply.github.com>
2024-02-04 23:19:39 +0100

    Added a function that converts a set to a vector

--- src/Utils.h
+++ src/Utils.h
@@ -16,10 +16,20 @@ public:
         return std::chrono::duration_cast<std::chrono::seconds>(clockNow.time_since_epoch()).count();
     }
 
-    // I love stack overflow <3 https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exists-using-standard-c-c11-14-17-c
-    static bool fileExists (const std::string& name) {
-        struct stat buffer{};
+    // I love stack overflow <3
+    // https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exists-using-standard-c-c11-14-17-c
+    static bool fileExists(const std::string &name) {
+        struct stat buffer {};
         return stat(name.c_str(), &buffer) == 0;
+    }
+
+    template<typename T>
+    static std::vector<T> copySetToVector(std::set<T> setToCopy) {
+        std::vector<T> vec;
+        for (auto element: setToCopy) {
+            vec.push_back(element);
+        }
+        return vec;
     }
 };
 #endif // GDDL_UTILS_H
