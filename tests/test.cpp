1a1dc5f3821ba589933ca23862f4d25a64bc0a82
b1rtek <53182944+B1rtek@users.noreply.github.com>
2024-02-29 01:57:37 +0100

    More test tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,11 +1,17 @@
 #include <GeodeTest/GeodeTest.h>
 #include "../src/RatingsManager.h"
 
-DEFINE_TEST_G(GDDLTest, TestName) {
+DEFINE_TEST_G(TestName, GDDLTest) {
     TEST_EQ(1, 2-1);
+    TEST_EQ(6, 9-3);
+    TEST_STR_EQ("btg", "btg");
 }
 
-DEFINE_TEST_G(GDDLTest, GDTest) {
+DEFINE_TEST_G(GDTest, GDDLTest) {
     auto color = RatingsManager::getTierColor(-1);
     TEST_EQ(color.r, 255);
+}
+
+DEFINE_TEST_G(FailingTest, GDDLTest) {
+    TEST_EQ(1, 0);
 }
