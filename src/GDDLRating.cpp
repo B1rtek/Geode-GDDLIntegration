fe0485a7e2e9055b549711be12f86d4ce7a3c879
b1rtek <53182944+B1rtek@users.noreply.github.com>
2024-06-22 22:48:03 +0200

    Fixed more mistakenly removed null checks

--- src/GDDLRating.cpp
+++ src/GDDLRating.cpp
@@ -1,9 +1,9 @@
 #include "GDDLRating.h"
 
 GDDLRating::GDDLRating(matjson::Value levelData) {
-    this->rating = levelData.contains("Rating") ? levelData["Rating"].as_double() : -1.0f;
-    this->enjoyment = levelData.contains("Enjoyment") ? levelData["Enjoyment"].as_double() : -1.0f;
-    this->deviation = levelData.contains("Deviation") ? levelData["Deviation"].as_double() : 0.0;
+    this->rating = levelData.contains("Rating") && !levelData["Rating"].is_null() ? levelData["Rating"].as_double() : -1.0f;
+    this->enjoyment = levelData.contains("Enjoyment") && !levelData["Enjoyment"].is_null() ? levelData["Enjoyment"].as_double() : -1.0f;
+    this->deviation = levelData.contains("Deviation") && !levelData["Deviation"].is_null() ? levelData["Deviation"].as_double() : 0.0;
     this->ratingCount = levelData["RatingCount"].as_int();
     this->enjoymentCount = levelData["EnjoymentCount"].as_int();
     this->submissionCount = levelData["SubmissionCount"].as_int();
