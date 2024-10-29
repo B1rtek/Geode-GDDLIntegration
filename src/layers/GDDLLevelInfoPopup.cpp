8da0ff363ce6c639d40f616b5c1b27c47f360ff4
b1rtek <53182944+B1rtek@users.noreply.github.com>
2024-10-29 23:34:46 +0100

    Reverted the changes made to GDDLLevelInfoPopup

--- src/layers/GDDLLevelInfoPopup.cpp
+++ src/layers/GDDLLevelInfoPopup.cpp
@@ -4,9 +4,9 @@
 
 #include "RatingsManager.h"
 #include "Utils.h"
-#include "nodes/BarChartNode.h"
 
-bool GDDLLevelInfoPopup::init() {
+bool GDDLLevelInfoPopup::init()
+{
     if(!FLAlertLayer::init(150)) return false;
 
     const auto winSize = CCDirector::sharedDirector()->getWinSize();
@@ -33,7 +33,7 @@ bool GDDLLevelInfoPopup::init() {
     title->setPosition({0.0f, popupSize.y/2-27.5f}); // or basically the standard position below the top border for a title
 
     // content
-    // addLevelInfo();
+    addLevelInfo();
 
     // open in browser button
     const auto browserButtonSprite = CCSprite::createWithSpriteFrameName("GJ_redoBtn_001.png");
@@ -41,30 +41,17 @@ bool GDDLLevelInfoPopup::init() {
     const auto openInBrowserButton = CCMenuItemSpriteExtra::create(browserButtonSprite, this, menu_selector(GDDLLevelInfoPopup::onOpenInBrowser));
     openInBrowserButton->setID("gddl-level-info-browser"_spr);
     m_buttonMenu->addChild(openInBrowserButton, 9);
-    openInBrowserButton->setPosition({-100.0f, -(popupSize.y)/2.0f});
+    openInBrowserButton->setPosition({50.0f, -(popupSize.y)/2.0f + 30.0f});
 
     // ok button
     const auto spr = ButtonSprite::create("OK");
     const auto okButton = CCMenuItemSpriteExtra::create(spr, this, menu_selector(GDDLLevelInfoPopup::onClose));
     okButton->setID("gddl-level-info-ok"_spr);
     m_buttonMenu->addChild(okButton, 10);
-    okButton->setPosition({-130.0f, -(popupSize.y)/2.0f}); // or basically the standard position above the bottom border for a button
+    okButton->setPosition({0.0f, -(popupSize.y)/2.0f + 30.0f}); // or basically the standard position above the bottom border for a button
 
-    prepareSpreadListener();
-
-    if (RatingsManager::hasSpread(levelID)) {
-        addBarCharts();
-    } else {
-        const auto reqInProgress = CCLabelBMFont::create("Downloading spread...", "chatFont.fnt");
-        reqInProgress->setPosition({-100.0f, -40.0f});
-        reqInProgress->setID("gddl-level-info-spread-label"_spr);
-        m_buttonMenu->addChild(reqInProgress);
-        auto req = web::WebRequest();
-        spreadListener.setFilter(req.get(getSpreadEndpointUrl(levelID)));
-    }
-
-    m_buttonMenu->setPosition({m_buttonMenu->getPositionX(), m_buttonMenu->getPositionY() + 100.0f});
-
+    m_buttonMenu->updateLayout();
+    m_mainLayer->updateLayout();
     return true;
 }
 
@@ -76,36 +63,6 @@ void GDDLLevelInfoPopup::onClose(CCObject *sender) {
 void GDDLLevelInfoPopup::onOpenInBrowser(CCObject *sender) {
     std::string url = "https://gdladder.com/level/" + std::to_string(levelID);
     web::openLinkInBrowser(url);
-}
-
-void GDDLLevelInfoPopup::prepareSpreadListener() {
-    spreadListener.bind([this] (web::WebTask::Event* e) {
-        if (web::WebResponse* res = e->getValue()) {
-            const auto jsonResponse = res->json().unwrapOr(matjson::Value());
-            const RatingsSpread spread = RatingsSpread(jsonResponse);
-            RatingsManager::cacheSpread(this->levelID, spread);
-            addBarCharts();
-        } else if (e->isCancelled()) {
-            // :(
-        }
-    });
-}
-
-void GDDLLevelInfoPopup::addBarCharts() {
-    RatingsSpread spread = RatingsManager::getSpread(levelID);
-    const auto diffSpreadData = spread.getDiffSpreadData();
-    const auto diffChart = BarChartNode::create(diffSpreadData, {150.0f, 20.0f * diffSpreadData.size()}, 50.0f, 20.0f);
-    diffChart->setPosition({-230.0f, -150.0f});
-    const auto enjSpreadData = spread.getEnjSpreadData();
-    const auto enjChart = BarChartNode::create(enjSpreadData, {150.0f, 20.0f * enjSpreadData.size()}, 50.0f, 20.0f);
-    enjChart->setPosition({0.0f, -200.0f});
-    m_buttonMenu->removeChildByID("gddl-level-info-spread-label"_spr);
-    m_buttonMenu->addChild(diffChart);
-    m_buttonMenu->addChild(enjChart);
-}
-
-std::string GDDLLevelInfoPopup::getSpreadEndpointUrl(const int levelID) {
-    return "https://gdladder.com/api/level/" + std::to_string(levelID) + "/submissions/spread";
 }
 
 GDDLLevelInfoPopup *GDDLLevelInfoPopup::create(int levelID) {
@@ -140,3 +97,4 @@ void GDDLLevelInfoPopup::addLevelInfo() {
     m_buttonMenu->addChild(levelInfoLabels, 2);
     levelInfoLabels->setPosition({0.0f, 5.0f});
 }
+
