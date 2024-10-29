d1165c6632d91fed34c7c4f1a715f3de6baa2732
B1rtek <53182944+B1rtek@users.noreply.github.com>
2024-10-29 19:25:54 +0100

    Added bar charts to the LevelInfoPopup (ui is very wip)

--- src/layers/GDDLLevelInfoPopup.cpp
+++ src/layers/GDDLLevelInfoPopup.cpp
@@ -6,8 +6,7 @@
 #include "Utils.h"
 #include "nodes/BarChartNode.h"
 
-bool GDDLLevelInfoPopup::init()
-{
+bool GDDLLevelInfoPopup::init() {
     if(!FLAlertLayer::init(150)) return false;
 
     const auto winSize = CCDirector::sharedDirector()->getWinSize();
@@ -34,7 +33,7 @@ bool GDDLLevelInfoPopup::init()
     title->setPosition({0.0f, popupSize.y/2-27.5f}); // or basically the standard position below the top border for a title
 
     // content
-    addLevelInfo();
+    // addLevelInfo();
 
     // open in browser button
     const auto browserButtonSprite = CCSprite::createWithSpriteFrameName("GJ_redoBtn_001.png");
@@ -42,29 +41,30 @@ bool GDDLLevelInfoPopup::init()
     const auto openInBrowserButton = CCMenuItemSpriteExtra::create(browserButtonSprite, this, menu_selector(GDDLLevelInfoPopup::onOpenInBrowser));
     openInBrowserButton->setID("gddl-level-info-browser"_spr);
     m_buttonMenu->addChild(openInBrowserButton, 9);
-    openInBrowserButton->setPosition({50.0f, -(popupSize.y)/2.0f + 30.0f});
+    openInBrowserButton->setPosition({-100.0f, -(popupSize.y)/2.0f});
 
     // ok button
     const auto spr = ButtonSprite::create("OK");
     const auto okButton = CCMenuItemSpriteExtra::create(spr, this, menu_selector(GDDLLevelInfoPopup::onClose));
     okButton->setID("gddl-level-info-ok"_spr);
     m_buttonMenu->addChild(okButton, 10);
-    okButton->setPosition({0.0f, -(popupSize.y)/2.0f + 30.0f}); // or basically the standard position above the bottom border for a button
+    okButton->setPosition({-130.0f, -(popupSize.y)/2.0f}); // or basically the standard position above the bottom border for a button
 
-    const std::vector<BarChartData> barChartData = {
-            {ccc4f(1.0f, 0.0f, 0.0f, 1.0f), "Tier 69", 7},
-            {ccc4f(0.0f, 1.0f, 0.0f, 1.0f), "Tier 420", 5},
-            {ccc4f(0.0f, 0.0f, 1.0f, 1.0f), "Tier 1337", 14},
-            {ccc4f(1.0f, 1.0f, 0.0f, 1.0f), "Tier 1338", 2},
-            {ccc4f(1.0f, 0.859f, 0.529f, 1.0f), "Tier 80085", 3}
-    };
+    prepareSpreadListener();
 
-    const auto barChart = BarChartNode::create(barChartData, {150.0f, 100.0f}, 50.0f, 20.0f);
-    barChart->setPosition({-100.0f, -180.0f});
-    m_buttonMenu->addChild(barChart);
+    if (RatingsManager::hasSpread(levelID)) {
+        addBarCharts();
+    } else {
+        const auto reqInProgress = CCLabelBMFont::create("Downloading spread...", "chatFont.fnt");
+        reqInProgress->setPosition({-100.0f, -40.0f});
+        reqInProgress->setID("gddl-level-info-spread-label"_spr);
+        m_buttonMenu->addChild(reqInProgress);
+        auto req = web::WebRequest();
+        spreadListener.setFilter(req.get(getSpreadEndpointUrl(levelID)));
+    }
 
-    m_buttonMenu->updateLayout();
-    m_mainLayer->updateLayout();
+    m_buttonMenu->setPosition({m_buttonMenu->getPositionX(), m_buttonMenu->getPositionY() + 100.0f});
+
     return true;
 }
 
@@ -76,6 +76,36 @@ void GDDLLevelInfoPopup::onClose(CCObject *sender) {
 void GDDLLevelInfoPopup::onOpenInBrowser(CCObject *sender) {
     std::string url = "https://gdladder.com/level/" + std::to_string(levelID);
     web::openLinkInBrowser(url);
+}
+
+void GDDLLevelInfoPopup::prepareSpreadListener() {
+    spreadListener.bind([this] (web::WebTask::Event* e) {
+        if (web::WebResponse* res = e->getValue()) {
+            const auto jsonResponse = res->json().unwrapOr(matjson::Value());
+            const RatingsSpread spread = RatingsSpread(jsonResponse);
+            RatingsManager::cacheSpread(this->levelID, spread);
+            addBarCharts();
+        } else if (e->isCancelled()) {
+            // :(
+        }
+    });
+}
+
+void GDDLLevelInfoPopup::addBarCharts() {
+    RatingsSpread spread = RatingsManager::getSpread(levelID);
+    const auto diffSpreadData = spread.getDiffSpreadData();
+    const auto diffChart = BarChartNode::create(diffSpreadData, {150.0f, 20.0f * diffSpreadData.size()}, 50.0f, 20.0f);
+    diffChart->setPosition({-230.0f, -150.0f});
+    const auto enjSpreadData = spread.getEnjSpreadData();
+    const auto enjChart = BarChartNode::create(enjSpreadData, {150.0f, 20.0f * enjSpreadData.size()}, 50.0f, 20.0f);
+    enjChart->setPosition({0.0f, -200.0f});
+    m_buttonMenu->removeChildByID("gddl-level-info-spread-label"_spr);
+    m_buttonMenu->addChild(diffChart);
+    m_buttonMenu->addChild(enjChart);
+}
+
+std::string GDDLLevelInfoPopup::getSpreadEndpointUrl(const int levelID) {
+    return "https://gdladder.com/api/level/" + std::to_string(levelID) + "/submissions/spread";
 }
 
 GDDLLevelInfoPopup *GDDLLevelInfoPopup::create(int levelID) {
--- src/layers/GDDLLevelInfoPopup.h
+++ src/layers/GDDLLevelInfoPopup.h
@@ -2,18 +2,25 @@
 #define GDDLINTEGRATION_GDDLLEVELINFOPOPUP_H
 
 #include <Geode/Bindings.hpp>
+#include <Geode/utils/web.hpp>
+
 
 using namespace geode::prelude;
 
 class GDDLLevelInfoPopup final : public FLAlertLayer {
+    EventListener<web::WebTask> spreadListener;
+    int levelID;
+    static constexpr CCPoint popupSize = {500.0f, 340.0f};
+
     explicit GDDLLevelInfoPopup(int levelId);
     bool init() override;
     void onClose(CCObject *sender);
     void onOpenInBrowser(CCObject *sender);
 
-    int levelID;
-    const inline static CCPoint popupSize = {300.0f, 160.0f};
+    void prepareSpreadListener();
+    void addBarCharts();
     void addLevelInfo();
+    static std::string getSpreadEndpointUrl(const int levelID);
 public:
     static GDDLLevelInfoPopup* create(int levelID);
     void show() override;
