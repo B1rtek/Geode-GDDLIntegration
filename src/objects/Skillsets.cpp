32a94fd744102e70a0a991711c9a8be10d9944a6
b1rtek <53182944+B1rtek@users.noreply.github.com>
2024-11-01 02:09:31 +0100

    Added the skillsets list

--- src/objects/Skillsets.cpp
+++ src/objects/Skillsets.cpp
@@ -3,33 +3,27 @@
 #include <algorithm>
 #include <iterator>
 
-Skillsets::Skillsets(const matjson::Value& tagsJson) {
+Skillsets::Skillsets(const matjson::Value &tagsJson) {
     if (!tagsJson.is_array()) {
         return;
     }
-    const auto& tags = tagsJson.as_array();
-    for (const auto& tag : tags) {
-        if (!tag.contains("ReactCount") || !tag["ReactCount"].is_number() || !tag.contains("Tag") || !tag["Tag"].
-            is_object()) {
-            continue;
-        }
-        const auto& tagDetails = tag["Tag"];
-        if (!tagDetails.contains("Name") || !tagDetails["Name"].is_string()) {
+    const auto &tags = tagsJson.as_array();
+    for (const auto &tag: tags) {
+        if (!tag.contains("ReactCount") || !tag["ReactCount"].is_number() || !tag.contains("TagID") || !tag["TagID"].
+                is_number()) {
             continue;
         }
         const int votes = tag["ReactCount"].as_int();
-        const std::string name = tagDetails["Name"].as_string();
-        skillsets.emplace_back(name, votes);
+        const int tagID = tag["TagID"].as_int();
+        skillsets.emplace_back(tagID, votes);
     }
     std::ranges::sort(skillsets,
-                      [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
-                          return a.second < b.second;
-                      });
+                      [](const std::pair<int, int> &a, const std::pair<int, int> &b) { return a.second > b.second; });
 }
 
-std::vector<std::string> Skillsets::getSkillsets() const {
-    std::vector<std::string> justSkillsets;
-    for (const auto& skillsetPair : skillsets) {
+std::vector<int> Skillsets::getSkillsets() const {
+    std::vector<int> justSkillsets;
+    for (const auto &skillsetPair: skillsets) {
         justSkillsets.push_back(skillsetPair.first);
     }
     return justSkillsets;
