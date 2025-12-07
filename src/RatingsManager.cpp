e51f603f33ffb38751506c1b08030358dca90ae7
B1rtek <53182944+B1rtek@users.noreply.github.com>
2025-12-07 02:32:02 +0100

    Cache uses the defaultRating if it's impossible to use real rating

--- src/RatingsManager.cpp
+++ src/RatingsManager.cpp
@@ -173,7 +173,8 @@ bool RatingsManager::addRatingFromResponse(const int id, const std::string &resp
         return false;
     }
     demonMap[id] = rating;
-    ratingsCache[id] = rating.roundedRating;
+    const int correctedRating = rating.roundedRating != -1 ? rating.roundedRating : rating.defaultRating;
+    ratingsCache[id] = correctedRating;
     // the requests for ratings are being made inside the rating popups, so the rest of the interface has to "subscribe" to changes
     // this is the place where we can notify them about the update that happened to the ratings list
     for (const auto observer: ratingObservers) {
