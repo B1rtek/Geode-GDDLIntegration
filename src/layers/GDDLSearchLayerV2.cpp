df1bc017d9c0c608d4bdd1e8d180f393ca53adf3
B1rtek <53182944+B1rtek@users.noreply.github.com>
2026-02-11 00:09:06 +0100

    GDDLSearchLayerV2 respects the user window theme preference

--- src/layers/GDDLSearchLayerV2.cpp
+++ src/layers/GDDLSearchLayerV2.cpp
@@ -1,15 +1,22 @@
 #include "GDDLSearchLayerV2.h"
 
+#include <Utils.h>
 #include <Geode/utils/cocos.hpp>
 
 bool GDDLSearchLayerV2::init() {
     if (!FLAlertLayer::init(150))
         return false;
 
-    // window size and buttons boilerplate, to be upgraded with colors
+    createBaseUI();
+
+    return true;
+}
+
+void GDDLSearchLayerV2::createBaseUI() {
+    // window size and buttons boilerplate
     const auto winSize = CCDirector::sharedDirector()->getWinSize();
     // background
-    const auto bg = CCScale9Sprite::create("GJ_square01.png", {0.0f, 0.0f, 80.0f, 80.0f});
+    const auto bg = CCScale9Sprite::create(Utils::getGrayPopupBG().c_str(), {0.0f, 0.0f, 80.0f, 80.0f});
     bg->setContentSize(popupSize);
     bg->setPosition({winSize.width / 2, winSize.height / 2});
     bg->setID("gddl-demon-search-popup"_spr);
@@ -23,13 +30,11 @@ bool GDDLSearchLayerV2::init() {
     m_buttonMenu->setID("gddl-demon-search-menu"_spr);
     m_mainLayer->addChild(m_buttonMenu, 0);
     // close button
-    const auto closeButtonSprite = CCSprite::createWithSpriteFrameName("GJ_closeBtn_001.png");
+    const auto closeButtonSprite = Utils::getGrayPopupCloseButton();
     const auto closeButton =
             CCMenuItemSpriteExtra::create(closeButtonSprite, this, menu_selector(GDDLSearchLayerV2::onClose));
     m_buttonMenu->addChild(closeButton, 1);
     closeButton->setPosition({10.0f, 0.0f});
-
-    return true;
 }
 
 void GDDLSearchLayerV2::onClose(CCObject* sender) {
--- src/layers/GDDLSearchLayerV2.h
+++ src/layers/GDDLSearchLayerV2.h
@@ -9,9 +9,14 @@ using namespace geode::prelude;
 class GDDLSearchLayerV2 : public FLAlertLayer {
     const CCPoint popupSize = {440.0f, 290.0f};
 
+    // normal popup stuff
     bool init() override;
     void onClose(CCObject *sender);
+
+    // internal ui methods
+    void createBaseUI();
 public:
+    // normal popup stuff
     static GDDLSearchLayerV2* create();
     void show() override;
 };
