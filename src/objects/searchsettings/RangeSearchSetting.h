0127ccc183d93713edf0f1abf6c8a4b8d43d0c4a
B1rtek <53182944+B1rtek@users.noreply.github.com>
2026-02-21 02:43:53 +0100

    Finalized logic in RangeSearchSetting

--- src/objects/searchsettings/RangeSearchSetting.h
+++ src/objects/searchsettings/RangeSearchSetting.h
@@ -17,29 +17,23 @@ public:
 
     void setSettingValue(std::vector<T> value) override {
         if (value.size() != 2) return;
-        this->setMinValue(value[0]);
-        this->setMaxValue(value[1]);
+        this->setValueNumber(0, value[0]);
+        this->setValueNumber(1, value[1]);
     }
 
     void loadSetting() override {
-        SearchSetting<T>::loadSetting();
+        SearchSetting<std::vector<T>>::loadSetting();
         this->setSettingValue(this->value);
     }
 
-    void setMinValue(T value) {
-        const T newValue = value < minValue ? maxValue : value;
-        if (!allowOverlap && newValue > this->value[1]) return;
-        this->value[0] = newValue;
+    void setValueNumber(unsigned index, T value) {
+        T newValue = value < minValue ? maxValue : value > maxValue ? minValue : value;
+        if (!allowOverlap && newValue > this->value[1 - index]) newValue = this->value[1 - index];
+        this->value[index] = newValue;
     }
 
-    void setMaxValue(T value) {
-        const T newValue = value > maxValue ? minValue : value;
-        if (!allowOverlap && newValue < this->value[0]) return;
-        this->value[1] = newValue;
-    }
-
-    std::pair<bool, bool> isAtMinValue() {
-        return std::make_pair(this->value[0] == minValue, this->value[1] == minValue);
+    std::vector<T> getMinMaxValues() {
+        return {minValue, maxValue};
     }
 };
 
