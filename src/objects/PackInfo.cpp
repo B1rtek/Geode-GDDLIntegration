ef6c9c0162924087f6c5de80d475f76117fe6f8d
B1rtek <53182944+B1rtek@users.noreply.github.com>
2026-03-10 17:55:13 +0100

    PackInfo stores all important information about the pack itself for now

--- src/objects/PackInfo.cpp
+++ src/objects/PackInfo.cpp
@@ -1,17 +1,51 @@
 #include "PackInfo.h"
 
-PackInfo::PackInfo(const std::vector<int>& levels, const std::string& packName, const std::string& packIconPath) : levels(levels), packName(packName), packIconPath(packIconPath) {
+#include "PackInfo.h"
 
+#include <Geode/ui/Notification.hpp>
+
+PackInfo::PackInfo(const int id, const int categoryId, const std::string& name, const std::string& iconPath,
+                   const int medianTier): id(id), categoryId(categoryId), name(name), iconPath(iconPath), medianTier(medianTier) {
+}
+
+Result<PackInfo> PackInfo::createFromJson(const matjson::Value& json) {
+    if (!json.isObject() ||
+        !json.contains("ID") || !json["ID"].isNumber() ||
+        !json.contains("CategoryID") || !json["CategoryID"].isNumber() ||
+        !json.contains("Name") || !json["Name"].isString()) {
+        return Err("Invalid PackInfo JSON");
+    }
+    std::string iconName = "tier_unrated.png";
+    if (json.contains("IconName") && json["IconName"].isString()) {
+        iconName = json["IconName"].asString().unwrap();
+    }
+    int medianTier = -1;
+    if (json.contains("Meta") && json["Meta"].isObject() && json["Meta"].contains("MedianTier") && json["Meta"]["MedianTier"].isNumber()) {
+        medianTier = json["Meta"]["MedianTier"].asInt().unwrap();
+    }
+    return Ok(PackInfo(json["ID"].asInt().unwrap(), json["CategoryID"].asInt().unwrap(), json["Name"].asString().unwrap(), iconName, medianTier));
+}
+
+int PackInfo::getId() const {
+    return id;
+}
+
+int PackInfo::getCategoryId() const {
+    return categoryId;
+}
+
+std::string PackInfo::getName() const {
+    return name;
+}
+
+std::string PackInfo::getIconPath() const {
+    return iconPath;
+}
+
+int PackInfo::getMedianTier() const {
+    return medianTier;
 }
 
 std::vector<int> PackInfo::getLevels() const {
     return levels;
-}
-
-std::string PackInfo::getPackName() const {
-    return packName;
-}
-
-std::string PackInfo::getPackIconPath() const {
-    return packIconPath;
 }
--- src/objects/PackInfo.h
+++ src/objects/PackInfo.h
@@ -1,20 +1,30 @@
 #ifndef GDDLINTEGRATION_PACKINFO_H
 #define GDDLINTEGRATION_PACKINFO_H
 
-#include <string>
-#include <vector>
+#include <matjson.hpp>
+#include <Geode/utils/web.hpp>
+
+using namespace geode::prelude;
 
 class PackInfo {
-    const std::vector<int> levels;
-    const std::string packName;
-    const std::string packIconPath;
-
+    const int id;
+    const int categoryId;
+    const std::string name;
+    const std::string iconPath;
+    const int medianTier;
+    std::vector<int> levels;
+    
 public:
-    PackInfo(const std::vector<int>& levels, const std::string& packName, const std::string& packIconPath);
+    PackInfo(const int id, const int categoryId, const std::string& name, const std::string& iconPath, const int medianTier);
 
+    static Result<PackInfo> createFromJson(const matjson::Value& json);
+
+    int getId() const;
+    int getCategoryId() const;
+    std::string getName() const;
+    std::string getIconPath() const;
+    int getMedianTier() const;
     std::vector<int> getLevels() const;
-    std::string getPackName() const;
-    std::string getPackIconPath() const;
 };
 
 
