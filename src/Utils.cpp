6da063e2638e87fc87fa4d0ef22b978572367d60
B1rtek <53182944+B1rtek@users.noreply.github.com>
2026-09-09 20:55:20 +0200

    .unwrap() improvements

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -369,7 +369,8 @@ std::string Utils::getErrorFromMessageAndResponse(matjson::Value jsonResponse, w
     std::string errorMessage = getErrorMessageFromErrorCode(res.code()).value_or(res.string().unwrapOr(std::to_string(res.code())));
     if (jsonResponse.contains("message")) {
         if (jsonResponse["message"].isArray()) {
-            errorMessage = jsonResponse["message"].asArray().unwrap()[0].asString().unwrapOr("Response was not a valid string");
+            const auto errorsArray = jsonResponse["message"].asArray().unwrap();
+            errorMessage = errorsArray.empty() ? "Error not provided" : errorsArray[0].asString().unwrapOr("Response was not a valid string");
         } else if (jsonResponse["message"].isString()) {
             errorMessage = jsonResponse["message"].asString().unwrapOr("Response was not a valid string");
         }
