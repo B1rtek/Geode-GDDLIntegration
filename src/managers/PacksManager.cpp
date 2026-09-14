a39557eb9de82f4d58fe3f7a6f2bf2fb47be6c4f
B1rtek <53182944+B1rtek@users.noreply.github.com>
2026-09-15 01:15:47 +0200

    Updated for API v3

--- src/managers/PacksManager.cpp
+++ src/managers/PacksManager.cpp
@@ -329,8 +329,8 @@ Result<std::shared_ptr<PackInfo>> PacksManager::getPackLevelsFromJson(const matj
     updatedPackInfo->clearLevelList();
     for (const auto& levelObject : json.asArray().unwrap()) {
         if (levelObject.isObject() && levelObject.contains("LevelID") && levelObject["LevelID"].isNumber() &&
-            levelObject.contains("EX") && levelObject["EX"].isBool()) {
-            updatedPackInfo->addLevel(levelObject["LevelID"].asInt().unwrap(), levelObject["EX"].asBool().unwrap());
+            levelObject.contains("EX") && levelObject["EX"].isNumber()) {
+            updatedPackInfo->addLevel(levelObject["LevelID"].asInt().unwrap(), levelObject["EX"].asInt().unwrap() == 1);
         } else {
             // invalid json
             log::info("[PacksManager::getPackLevelsFromJson] Invalid level JSON object");
